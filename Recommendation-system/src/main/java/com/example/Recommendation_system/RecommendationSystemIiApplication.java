2fb5068235e138ace37d66c032fb09af17eb0e0b
zhe-v <zhe-valentin@mail.ru>
2025-04-20 11:57:35 +0700

    Recommendation-system II

---
+++ Recommendation-system/src/main/java/com/example/Recommendation_system/RecommendationSystemIiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Recommendation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationSystemIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationSystemIiApplication.class, args);
+	}
+
+}
